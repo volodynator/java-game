a6c1eff40c20d60096d1644940ab65f84d47dc90
Artem <mozharov.artemiy@gmail.com>
2024-09-12 15:35:52 +0200

    я щасливий

--- src/Platformer.java
+++ src/Platformer.java
@@ -11,14 +11,13 @@ import java.io.Serial;
 import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Timer;
 import javax.imageio.ImageIO;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
+import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class Platformer extends JFrame{
+public class Platformer extends JFrame {
 	@Serial
 	private static final long serialVersionUID = 5736902251450559962L;
 	Timer timer;
@@ -27,7 +26,6 @@ public class Platformer extends JFrame{
 	BufferStrategy bufferStrategy;
 	private boolean running = false;
 
-
 	private List<Bullet> bullets = new ArrayList<>();
 	private boolean notEnoughMana = false;
 
@@ -35,7 +33,7 @@ public class Platformer extends JFrame{
 	public List<Tile> levelObjects = new ArrayList<>();
 
 	public Platformer() {
-		//exit program when window is closed
+		// Exit program when window is closed
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
@@ -78,7 +76,6 @@ public class Platformer extends JFrame{
 		startTimer();
 	}
 
-
 	private void startTimer() {
 		timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask() {
@@ -89,12 +86,11 @@ public class Platformer extends JFrame{
 				player.update();
 				checkCollision();
 				updateGameStateAndRepaint();
-
+				((AL) getKeyListeners()[0]).updateMovement(); // Call updateMovement regularly
 			}
-		}, 0, 10);
-        paint(getGraphics());
+		}, 0, 10); // Adjust the timer as per game update speed
+		paint(getGraphics());
 	}
-
 
 	private void updateGameStateAndRepaint() {
 		l.update();
@@ -108,9 +104,7 @@ public class Platformer extends JFrame{
 			}
 		}
 		int playerCenterX = player.x + player.getImage().getWidth() / 2;
-
-//		System.out.println("Player : "+player.x);
-		int maxOffsetX = l.getResultingImage().getWidth(this) - this.getWidth();
+		int maxOffsetX = l.getResultingImage().getWidth(null) - this.getWidth();
 
 		l.offsetX = Math.max(0, Math.min(playerCenterX - this.getWidth() / 2, maxOffsetX));
 		repaint();
@@ -125,7 +119,7 @@ public class Platformer extends JFrame{
 			} catch (IOException e) {
 				throw new RuntimeException(e);
 			}
-		}finally {
+		} finally {
 			g2.dispose();
 		}
 		bufferStrategy.show();
@@ -133,73 +127,59 @@ public class Platformer extends JFrame{
 
 	private void draw(Graphics2D g2d) throws IOException {
 		BufferedImage img_level = (BufferedImage) l.getResultingImage();
-		BufferedImage visibleLevel =
-				img_level.getSubimage((int) l.offsetX, 0, 1000, l.getHeight());
+		BufferedImage visibleLevel = img_level.getSubimage((int) l.offsetX, 0, 1000, l.getHeight());
 		g2d.drawImage(visibleLevel, 0, 0, this);
-		g2d.drawImage(player.getImage(), player.x, player.y, this);
+		g2d.drawImage(player.getImage(), player.x - (int) l.offsetX, player.y, this);
 
-		// bullets
-		for (Bullet bullet: bullets) {
+		// Bullets
+		for (Bullet bullet : bullets) {
 			g2d.drawImage(bullet.image, bullet.x, bullet.y, this);
 		}
-		// hp
+		// HP
 		g2d.setColor(Color.RED);
 		g2d.drawRoundRect(50, 50, 300, 20, 4, 4);
-		g2d.fillRoundRect(50, 50, player.hp*3, 20, 4, 4);
+		g2d.fillRoundRect(50, 50, player.hp * 3, 20, 4, 4);
 
-		// mana
+		// Mana
 		g2d.setColor(Color.BLUE);
 		g2d.drawRoundRect(50, 90, 300, 20, 4, 4);
-		g2d.fillRoundRect(50, 90, player.mana*3, 20, 4, 4);
-		if (notEnoughMana){
+		g2d.fillRoundRect(50, 90, player.mana * 3, 20, 4, 4);
+		if (notEnoughMana) {
 			g2d.setColor(Color.RED);
-			g2d.fillRoundRect(50, 90, player.mana*3, 20, 4, 4);
-			notEnoughMana=false;
+			g2d.fillRoundRect(50, 90, player.mana * 3, 20, 4, 4);
+			notEnoughMana = false;
 		}
-		// skills
+		// Skills
 		g2d.setColor(Color.RED);
 		int space = 0;
-		for (int i = 0; i<4; i++) {
-			if (i==selectedItem){
+		for (int i = 0; i < 4; i++) {
+			if (i == selectedItem) {
 				g2d.setColor(Color.GREEN);
 			}
-			g2d.drawRoundRect(650+space, 50, 50, 50, 4, 4);
-			g2d.drawImage(player.itemsList.get(i).icon, 650+space, 50, 50, 50, this);
-			space+=70;
+			g2d.drawRoundRect(650 + space, 50, 50, 50, 4, 4);
+			g2d.drawImage(player.itemsList.get(i).icon, 650 + space, 50, 50, 50, this);
+			space += 70;
 			g2d.setColor(Color.RED);
 		}
-		// shield
-		if (player.shield!=null){
-			g2d.setColor(Color.BLUE);
-			int playerCenterX = player.x + player.getImage().getWidth() / 2;
-			int playerCenterY = player.y + player.getImage().getHeight() / 2;
-			g2d.drawOval(playerCenterX - 150, playerCenterY-150, 300, 300);
+		// Shield
+		if (player.shield != null) {
 		}
 	}
 
-
-	private void checkCollision(){
+	private void checkCollision() {
 		BoundingBox.ColisionType colisionType = BoundingBox.ColisionType.NONE;
 
-		for(Tile tile : levelObjects){
-			switch(tile.tileIndex){
+		for (Tile tile : levelObjects) {
+			switch (tile.tileIndex) {
 				case 0: {
 					colisionType = player.getBoundingBox().checkColision(tile.getBoundingBox());
-					switch(colisionType){
+					switch (colisionType) {
 						case DOWN -> {
 							player.inAir = false;
 							return;
 						}
-
 					}
 				}
-//				case 1: {
-//					colisionType = player.getBoundingBox().checkColision(tile.getBoundingBox());
-//					if(colisionType != BoundingBox.ColisionType.NONE){
-//						System.out.println("restart");
-//						player.restart();
-//					}
-//				}
 			}
 		}
 
@@ -207,21 +187,21 @@ public class Platformer extends JFrame{
 		if (player.y > l.getHeight()) {
 			player.restart();
 		}
-
 	}
+
 	@Override
 	public void dispose() {
-
 		if (timer != null) {
 			timer.cancel();
 		}
 		super.dispose();
 	}
 
-
-
 	public class AL extends KeyAdapter {
 		Platformer p;
+		private boolean leftPressed = false;
+		private boolean rightPressed = false;
+		private boolean jumpPressed = false;
 
 		public AL(Platformer p) {
 			super();
@@ -237,37 +217,71 @@ public class Platformer extends JFrame{
 			}
 
 			if (keyCode == KeyEvent.VK_LEFT) {
-				player.move("left");
+				leftPressed = true;
 			}
 
 			if (keyCode == KeyEvent.VK_RIGHT) {
-				player.move("right");
-			}
-			if (keyCode == KeyEvent.VK_SPACE) {
-				player.jump();
+				rightPressed = true;
 			}
 
-			if (keyCode == KeyEvent.VK_E){
+			if (keyCode == KeyEvent.VK_SPACE || keyCode == KeyEvent.VK_K) {
+				jumpPressed = true;
+			}
+
+			if (keyCode == KeyEvent.VK_E) {
 				try {
-					System.out.println("Mana: "+player.mana);
+					System.out.println("Mana: " + player.mana);
 					player.itemsList.get(selectedItem).use();
 					bullets.addAll(player.bullets);
 				} catch (IOException | NotEnoughManaExeption e) {
 					notEnoughMana = true;
 					player.playSound("assets/Sound/button-18.wav");
 				}
-            }
+			}
+
 			if (keyCode == KeyEvent.VK_1) {
-				selectedItem=0;
+				selectedItem = 0;
 			}
+
 			if (keyCode == KeyEvent.VK_2) {
-				selectedItem=1;
+				selectedItem = 1;
 			}
+
 			if (keyCode == KeyEvent.VK_3) {
-				selectedItem=2;
+				selectedItem = 2;
 			}
+
 			if (keyCode == KeyEvent.VK_4) {
-				selectedItem=3;
+				selectedItem = 3;
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent event) {
+			int keyCode = event.getKeyCode();
+
+			if (keyCode == KeyEvent.VK_LEFT) {
+				leftPressed = false;
+			}
+
+			if (keyCode == KeyEvent.VK_RIGHT) {
+				rightPressed = false;
+			}
+
+			if (keyCode == KeyEvent.VK_SPACE || keyCode == KeyEvent.VK_K) {
+				jumpPressed = false;
+			}
+		}
+
+		public void updateMovement() {
+			if (leftPressed) {
+				player.move("left");
+			}
+			if (rightPressed) {
+				player.move("right");
+			}
+			if (jumpPressed) {
+				player.jump();
 			}
 		}
 	}
